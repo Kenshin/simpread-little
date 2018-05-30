af95e1b3fe51f59b592a38de15922a94f9cdd9da
Kenshin <fxblog@gmail.com>
2018-05-30 11:07:47 +0800

    Add theme mobile style.

--- src/bookmarklet/simpread.js
+++ src/bookmarklet/simpread.js
@@ -14,6 +14,7 @@ const notif_style  = "https://raw.githubusercontent.com/kenshin/simpread-little/
       user_style   = "https://raw.githubusercontent.com/kenshin/simpread-little/develop/src/bookmarklet/res/simpread_user.css",
       option_style = "https://raw.githubusercontent.com/kenshin/simpread-little/develop/src/bookmarklet/res/option.css",
       theme_common = "https://raw.githubusercontent.com/kenshin/simpread-little/develop/src/bookmarklet/res/theme_common.css",
+      theme_mobile = "https://raw.githubusercontent.com/kenshin/simpread-little/develop/src/bookmarklet/res/theme_mobile.css",
       theme_pixyii = "https://raw.githubusercontent.com/kenshin/simpread-little/develop/src/bookmarklet/res/theme_pixyii.css",
       theme_gothic = "https://raw.githubusercontent.com/kenshin/simpread-little/develop/src/bookmarklet/res/theme_gothic.css",
       theme_night  = "https://raw.githubusercontent.com/kenshin/simpread-little/develop/src/bookmarklet/res/theme_night.css";
@@ -82,8 +83,8 @@ function platform() {
 function setStyle( style ) {
     if ( userAgent() == "iphone" ) {
         style.FontSize( "62.5%" );
-        //$("sr-read").css({ "padding": "0 50px" });
         $.get( theme_gothic, result => { $("head").append( `<style type="text/css">${result}</style>` ) });
+        $.get( theme_mobile, result => { $("head").append( `<style type="text/css">${result}</style>` ) });
     } else {
         style.FontSize( "72%" );
         style.Layout( "10%" );

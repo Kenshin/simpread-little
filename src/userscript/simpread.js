26dd950e4f85af2a10dbdcfe230fe292b1d60638
Kenshin <fxblog@gmail.com>
2019-05-15 16:23:20 +0800

    Fix import adapter site url bug.

--- src/userscript/simpread.js
+++ src/userscript/simpread.js
@@ -970,7 +970,7 @@ function optionMode() {
                 new Notify().Render( `请勿在 https 下面使用此功能，请前往 http 的页面，如： <a href='http://kenshin.wang/blog/' target='_blank' >点击这里</a>` );
                 return;
             }
-            $.getJSON( "http://sr.ksria.cn/website_list_v3.json" + "?_=" + Math.round(+new Date()), result => {
+            $.getJSON( "http://sr.ksria.cn/website_list_v4.json" + "?_=" + Math.round(+new Date()), result => {
                 const count = pr.Addsites( result );
                 count == 0 ? new Notify().Render( "适配列表已同步至最新版本。" ) : new Notify().Render( 0, `适配列表已同步成功，本次新增 ${ count } 个站点。` );
             });
